0e792be9cac72ee2d85e8bb3e1a9505adc350923
Arthur Kowskii Croquebois <contact@kowskii.com>
2026-07-26 19:35:27 +0200

    easier to read naming

--- midiControl.cpp
+++ midiControl.cpp
@@ -12,16 +12,17 @@ enum class chordVoicing { FIRST, SECOND, THIRD };
 enum class shiftState { ON, OFF };
 
 struct musicalType{
-    tonic actualTonic = tonic::F;
-    layout actualLayout = layout::SCALED;
-    scaleType actualScale = scaleType::MINOR;
-    chordShape actualChord = chordShape::TRI;
-    chordVoicing actualVoicing = chordVoicing::FIRST;
+    tonic tonic = tonic::F;
+    layout layout = layout::SCALED;
+    scaleType scaleType = scaleType::MINOR;
+    chordShape chordShape = chordShape::OFF;
+    chordVoicing chordVoicing = chordVoicing::FIRST;
 };
 
 int main(){
 
-
+    musicalType actualState;
+    actualState.chordShape = chordShape::TRI;
 
 
     return 0;
