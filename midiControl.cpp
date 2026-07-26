260aaa9bfe1cbfdb07f06fb8a54fcbfc9d86ede4
Arthur Kowskii Croquebois <contact@kowskii.com>
2026-07-26 19:24:52 +0200

    struct musicalType

--- midiControl.cpp
+++ midiControl.cpp
@@ -4,51 +4,20 @@
 #include <cstdint>
 #include <cmath>
 
-enum class scaleType {
-    MAJOR,
-    MINOR
+enum class tonic { C, D, E, F, G, A, B };
+enum class layout { SCALED, CHROMATIC };
+enum class scaleType { MAJOR, MINOR };
+enum class chordShape { OFF, SUS2, TRI, SUS4, SIXTH, SEVENTH, NINTH, ELEVENTH, THIRTEENTH };
+enum class chordVoicing { FIRST, SECOND, THIRD };
+enum class shiftState { ON, OFF };
+
+struct musicalType{
+    tonic actualTonic = F;
+    layout actualLayout = SCALED;
+    scaleType actualScale = MINOR;
+    chordShape actualChord = TRI;
+    chordVoicing actualVoicing = FIRST;
 };
-
-enum class layout {
-    SCALED,
-    CHROMATIC
-};
-
-enum class tonic {
-    C,
-    D,
-    E,
-    F,
-    G,
-    A,
-    B
-};
-
-enum class chordShape {
-    OFF,
-    SUS2,
-    TRI,
-    SUS4,
-    SIXTH,
-    SEVENTH,
-    NINTH,
-    ELEVENTH,
-    THIRTEENTH
-};
-
-enum class shiftState {
-    ON,
-    OFF
-};
-
-enum class chordVoicing {
-    FIRST,
-    SECOND,
-    THIRD
-};
-
-
-
 
 int main(){
 
